dadc46f9d1b9d3100fb98f2f94da97753559a844
IlyamtPCJ <144455673+IlyamtPCJ@users.noreply.github.com>
2023-10-13 23:35:38 +0500

    Add files via upload

---
+++ src/components/ToDoApp.svelte
@@ -0,0 +1,48 @@
+<script>
+	import ToDoControls from "./ToDoControls.svelte";
+	import ToDoItem from "./ToDoItem.svelte";
+
+    const items = [
+        {
+            id: 1,
+            text: 'Some Todo',
+            isDone: false,
+        },
+        {
+            id: 2,
+            text: 'Some Todo 2',
+            isDone: false,
+        },
+    ]
+</script>
+<div class="todo-app">
+    <ToDoControls />
+    <div class="todo-app_field">
+        {#each items as item}
+            <ToDoItem id={item.id} text={item.text} isDone={item.isDone}/>
+        {/each}
+    </div>
+</div>
+
+<style>
+    .todo-app {
+        width: 700px;
+        height: 80%;
+        background: rgb(196, 193, 193);
+        border-radius: 15px;
+        padding: 40px;
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+    }
+    .todo-app_field {
+        background: white;
+        flex-grow: 1;
+        border-radius: 15px;
+        overflow-y: auto;
+        padding: 15px;
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+</style>
---
+++ src/components/ToDoItem.svelte
@@ -0,0 +1,37 @@
+<script>
+    // export let id;
+    export let text;
+    // export let isDone;
+</script>
+
+
+<div class="todo-item">
+    <input class="todo-item_check" type="checkbox" />
+    <span class="todo-item_text">{text}</span>
+    <button class="todo-item_btn">Del</button>
+</div>
+
+<style>
+    .todo-item {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 15px;
+        background: rgb(235, 100, 100);
+        border-radius: 15px;
+    }
+
+    .todo-item_check {
+
+    }
+    .todo-item_text {
+        flex-grow: 1;
+    }
+    .todo-item_btn {
+        border: none;
+        border-radius: 10px;
+        background-color: chocolate;
+        padding: 10px;
+        min-width: 100px;
+    }
+</style>
