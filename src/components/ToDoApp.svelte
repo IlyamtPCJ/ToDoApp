f55b6856e54a5f4eb3d1e46b86bd0e467a30c961
IlyamtPCJ <144455673+IlyamtPCJ@users.noreply.github.com>
2023-10-15 19:44:03 +0500

    Add files via upload

--- src/components/ToDoApp.svelte
+++ src/components/ToDoApp.svelte
@@ -1,25 +1,61 @@
 <script>
 	import ToDoControls from "./ToDoControls.svelte";
 	import ToDoItem from "./ToDoItem.svelte";
+        import { onMount } from "svelte";
+    
+    let items = [];
+    let id = 0;
 
-    const items = [
-        {
-            id: 1,
-            text: 'Some Todo',
-            isDone: false,
-        },
-        {
-            id: 2,
-            text: 'Some Todo 2',
-            isDone: false,
-        },
-    ]
+    onMount(() => {
+        if (localStorage.key('items')) {
+            items = JSON.parse(localStorage.getItem('items'));
+        }
+        if(items.length) {
+            items.forEach((i) => {
+                if(id < i.id) {
+                    id = i.id;
+                }
+            });
+            id++;
+        }
+    });
+
+    function onChangeStatus(event) {
+        const item = items.find((i) => i.id === event.detail.id);
+        item.isDone = !item.isDone;
+        items = items;
+        localStorage.setItem('items', JSON.stringify(items));
+    }
+
+    function onAddItem(event) {
+        const item = {
+            id: id++,
+            text: event.detail.text,
+            isDone: false
+        };
+        items.push(item);
+        items = items;
+        localStorage.setItem('items', JSON.stringify(items));
+    }
+
+    function onDeleteItem(event) {
+        const idx = items.findIndex(i=>i.id === event.detail.id);
+        items.splice(idx, 1);
+        items = items;
+        localStorage.setItem('items', JSON.stringify(items));
+    }
 </script>
+
 <div class="todo-app">
-    <ToDoControls />
+    <ToDoControls on:add={onAddItem}/>
     <div class="todo-app_field">
         {#each items as item}
-            <ToDoItem id={item.id} text={item.text} isDone={item.isDone}/>
+            <ToDoItem
+            id={item.id}
+            text={item.text}
+            bind:isDone={item.isDone}
+            on:change={onChangeStatus}
+            on:remove={onDeleteItem}/>
         {/each}
     </div>
 </div>
--- src/components/ToDoItem.svelte
+++ src/components/ToDoItem.svelte
@@ -1,14 +1,19 @@
 <script>
-    // export let id;
+    export let id;
     export let text;
-    // export let isDone;
+    export let isDone;
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
 </script>
 
 
-<div class="todo-item">
-    <input class="todo-item_check" type="checkbox" />
+<div class="todo-item" class:todo-item_done={isDone}>
+    <input class="todo-item_check" type="checkbox" on:change={dispatch('change', {id})}
+    checked={isDone}
+    />
     <span class="todo-item_text">{text}</span>
-    <button class="todo-item_btn">Del</button>
+    <button class="todo-item_btn" on:click={dispatch('remove', {id})}>Del</button>
 </div>
 
 <style>
@@ -21,9 +26,14 @@
         border-radius: 15px;
     }
 
-    .todo-item_check {
-
+    .todo-item_done {
+        background: rgb(96, 231, 96);
     }
+
+    .todo-item_done .todo-item_text {
+        text-decoration: line-through;
+    }
+
     .todo-item_text {
         flex-grow: 1;
     }
